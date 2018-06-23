4a59bcebc52c531096be78c63ba8f65131cd2190
Ole Priebusch <opriebusch@web.de>
2018-06-23 03:00:42 +0200

    added a solution to the recursive staircase problem

---
+++ go/recursive_staircase_problem/recursive_staircase_problem.go
@@ -0,0 +1,52 @@
+package recursive_staircase_problem
+
+import (
+	"fmt"
+	"strings"
+)
+
+type StepSequence []int
+
+func (seq *StepSequence) String() string {
+	builder := strings.Builder{}
+
+	builder.WriteString("[")
+	for _, step := range *seq {
+		builder.WriteString(fmt.Sprintf("%v ", step))
+	}
+	builder.WriteString("]")
+
+	return builder.String()
+}
+
+func (seq *StepSequence) Copy() StepSequence {
+	copy := make(StepSequence, len(*seq))
+	for i := 0; i < len(*seq); i++ {
+		copy[i] = []int(*seq)[i]
+	}
+
+	return copy
+}
+
+func NumWaysWithStepSequences(N int, X []int, stepSequence StepSequence) (int, []StepSequence) {
+	if N == 0 {
+		return 1, []StepSequence{stepSequence}
+	}
+
+	var stepSequencesToAdd []StepSequence
+	var stepSequences []StepSequence
+	var numberOfPathesFromHere int
+	numberOfPathes := 0
+
+	for _, x := range X {
+		if x <= N {
+			numberOfPathesFromHere, stepSequencesToAdd = NumWaysWithStepSequences(N - x, X, append(stepSequence.Copy(), x))
+			numberOfPathes += numberOfPathesFromHere
+			for _, stepSequenceToAdd := range stepSequencesToAdd {
+				stepSequences = append(stepSequences, stepSequenceToAdd)
+			}
+		}
+	}
+
+	return numberOfPathes, stepSequences
+}
